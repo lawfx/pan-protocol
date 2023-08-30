4b361222fe49c2bc298dbdb59849f7cf92c71d66
Nikolaos Ioannou <ioannounikos93@gmail.com>
2023-08-30 22:08:23 +0200

    moved generate document function to utils class

--- src/components/DocumentGenerator/DocumentGenerator.tsx
+++ src/components/DocumentGenerator/DocumentGenerator.tsx
@@ -1,9 +1,4 @@
-import Docxtemplater from "docxtemplater";
-import saveAs from "file-saver";
-import PizZip from "pizzip";
-import { DOCX_MIME_TYPE } from "../../constants/constants";
-import { DocumentData } from "../../models/document-data.model";
-import { compileDocumentData } from "../../utils/utils";
+import { generateDocument } from "../../utils/utils";
 import Button from "../Button/Button";
 import { UserData } from "../../models/user-data.model";
 import { GitHubContext } from "../GithubProvider/GithubProvider";
@@ -19,35 +14,12 @@ export default function DocumentGenerator({ file, userData }:
 
   const canGenerateDocx = !!userData.name && !!userData.position && !!userData.date && !!userData.hours && !!file;
 
-  function generateDocument() {
-    const docData: DocumentData = compileDocumentData(userData, commits);
-    console.log(docData);
-
-    const zip = new PizZip(file);
-    const doc = new Docxtemplater(zip, {
-      paragraphLoop: true,
-      linebreaks: true,
-    });
-
-    doc.render({
-      name: docData.userData.name,
-      position: docData.userData.position,
-      date: docData.userData.date,
-      hours: docData.userData.hours,
-      prs: docData.commits.map(c => ({ title: c.message, num: c.prNum, sha: c.sha, hour: 5 }))
-    });
-
-    const blob = doc.getZip().generate({
-      type: "blob",
-      mimeType: DOCX_MIME_TYPE,
-      compression: "DEFLATE",
-    });
-
-    saveAs(blob, `${docData.userData.name}_${docData.userData.date}_procotol.docx`);
+  function handleClickGenerate() {
+    generateDocument(file, userData, commits);
   }
 
   return (
-    <Button onClick={generateDocument} disabled={!canGenerateDocx}>
+    <Button onClick={handleClickGenerate} disabled={!canGenerateDocx}>
       Generate
     </Button>
   );
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,10 @@ import { format } from "date-fns";
 import { CommitInfo } from "../models/commit.model";
 import { DocumentData } from "../models/document-data.model";
 import { UserData } from "../models/user-data.model";
+import Docxtemplater from "docxtemplater";
+import saveAs from "file-saver";
+import PizZip from "pizzip";
+import { DOCX_MIME_TYPE } from "../constants/constants";
 
 export function parseGithubCommitMessage(message: string): { message: string, prNum: string } {
   const messageData = message.match(/(?<title>.+) (\(#(?<num>\d+)\))/);
@@ -27,4 +31,32 @@ export function compileDocumentData(data: UserData, commits: CommitInfo[]): Docu
     }
     ).filter(r => !!r)
   }
+}
+
+export function generateDocument(file: string | ArrayBuffer | null, userData: UserData, commits: CommitInfo[]) {
+  if (!file) return;
+
+  const docData: DocumentData = compileDocumentData(userData, commits);
+
+  const zip = new PizZip(file);
+  const doc = new Docxtemplater(zip, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+  doc.render({
+    name: docData.userData.name,
+    position: docData.userData.position,
+    date: docData.userData.date,
+    hours: docData.userData.hours,
+    prs: docData.commits.map(c => ({ title: c.message, num: c.prNum, sha: c.sha, hour: 5 }))
+  });
+
+  const blob = doc.getZip().generate({
+    type: "blob",
+    mimeType: DOCX_MIME_TYPE,
+    compression: "DEFLATE",
+  });
+
+  saveAs(blob, `${docData.userData.name}_${docData.userData.date}_procotol.docx`);
 }
