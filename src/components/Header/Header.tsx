58095a08f1d78f4759737e380dd932e630ef59a1
Nikolaos Ioannou <ioannounikos93@gmail.com>
2023-09-03 13:27:18 +0200

    added a separator for change theme icon and login stuff

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <StyledSection>
       <Title>{APP_TITLE}</Title>
       <RightSide>
-        <ThemeToggleIcon />
+        <ThemeToggleIcon /> |
         <Login />
       </RightSide>
     </StyledSection>
